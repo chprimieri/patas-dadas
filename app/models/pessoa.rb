737085be21a52f2ffda9a7266ef7646639d5ea03
Camila Primieri <camilaprimieri@gmail.com>
2019-07-09 00:02:01 -0300

    validações no model pessoas

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,2 +1,8 @@
 class Pessoa < ApplicationRecord
+	validates :nome, :data_de_nascimento, :email, :telefone, presence: true
+	validates :email, uniqueness: true
+	validates :foto, allow_blank: true, format: {
+		with: %r{\.(jpg|png)\Z}i,
+		message: 'Deve ser uma imagem JPG ou PNG.'
+	}
 end
