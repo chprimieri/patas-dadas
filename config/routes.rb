1a6695b204a0f30573e8c0954cb9018eee06b136
Camila Primieri <camilaprimieri@gmail.com>
2019-07-11 09:15:28 -0300

    arrumado o menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :passeios
-  resources :cachorros
 	get 'home/index'
   root 'home#index'
-  
+
+  resources :passeios
+  resources :cachorros
   resources :pessoas
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
