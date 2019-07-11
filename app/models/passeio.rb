ba3431a979d16bf99e2401d272c62e8380345f0e
Camila Primieri <camilaprimieri@gmail.com>
2019-07-11 12:50:43 -0300

    arrumando a exibição das chaves estrangeiras no index de Passeio

--- app/models/passeio.rb
+++ app/models/passeio.rb
@@ -1,6 +1,6 @@
 class Passeio < ApplicationRecord
-  belongs_to :pessoa
-  belongs_to :cachorro
+  belongs_to :pessoa, class_name: "Pessoa", foreign_key: "pessoa_id"
+  belongs_to :cachorro, class_name: "Cachorro", foreign_key: "cachorro_id"
 
   validates :pessoa_id, :cachorro_id, :status, :data_e_hora, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,41 @@ I18n.reload!
 
 Faker::Config.locale = 'pt-BR'
 
-Pessoa.destroy_all
+# Pessoa.destroy_all
 
-50.times do |index|
-  Pessoa.create!( nome: Faker::Name.unique.name,
-  								data_de_nascimento: Faker::Date.birthday(18, 65),
-                  email: Faker::Internet.unique.email,
-                  telefone: Faker::PhoneNumber.phone_number,
-                  foto: "pessoa.jpg")
-end
+# 50.times do |index|
+#   Pessoa.create!( nome: Faker::Name.unique.name,
+#   								data_de_nascimento: Faker::Date.birthday(18, 65),
+#                   email: Faker::Internet.unique.email,
+#                   telefone: Faker::PhoneNumber.phone_number,
+#                   foto: "pessoa.jpg")
+# end
 
-p "Criadas #{Pessoa.count} pessoas"
+# p "Criadas #{Pessoa.count} pessoas"
 
-Cachorro.destroy_all
+# Cachorro.destroy_all
 
-50.times do |index|
-  Cachorro.create!( nome: Faker::Creature::Dog.unique.name,
-  								data_de_nascimento: Faker::Date.birthday(0, 15),
-                  sexo: Faker::Creature::Dog.gender,
-                  porte: Faker::Creature::Dog.size,
+# 50.times do |index|
+#   Cachorro.create!( nome: Faker::Creature::Dog.unique.name,
+#   								data_de_nascimento: Faker::Date.birthday(0, 15),
+#                   sexo: Faker::Creature::Dog.gender,
+#                   porte: Faker::Creature::Dog.size,
+#                   status: Faker::Subscription.status,
+#                   observacoes: Faker::Lorem.paragraph,
+#                   equipamento: Faker::Hipster.word,
+#                   frequencia: Faker::Number.between(3, 5),
+#                   duracao: Faker::Number.between(30, 60),
+#                   areas: Faker::Address.community,
+#                   disponivel_para_passeio: Faker::Boolean.boolean)
+# end
+
+Passeio.destroy_all
+
+200.times do |index|
+  Passeio.create!( pessoa_id: Faker::Number.between(1, 50),
+                  cachorro_id: Faker::Number.between(1, 50),
                   status: Faker::Subscription.status,
-                  observacoes: Faker::Lorem.paragraph,
-                  equipamento: Faker::Hipster.word,
-                  frequencia: Faker::Number.between(3, 5),
-                  duracao: Faker::Number.between(30, 60),
-                  areas: Faker::Address.community,
-                  disponivel_para_passeio: Faker::Boolean.boolean)
+                  data_e_hora: Faker::Time.between(DateTime.now - 30, DateTime.now + 30))
 end
 
-p "Criados #{Cachorro.count} cachorros"
+p "Criados #{Passeio.count} passeios"
