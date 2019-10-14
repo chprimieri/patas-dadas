673b428b3a4b9666f3b581e003f22c390ad3e30e
Camila Primieri <camilaprimieri@gmail.com>
2019-10-13 23:25:41 -0300

    !

--- app/models/cachorro.rb
+++ app/models/cachorro.rb
@@ -1,4 +1,6 @@
 class Cachorro < ApplicationRecord
+	enum status: [:ativo, :inativo]
+
 	validates :nome, :data_de_nascimento, :sexo, :porte, :frequencia, :duracao, presence: true
 	validates :foto, allow_blank: true, format: {
 		with: %r{\.(jpg|png)\Z}i,
